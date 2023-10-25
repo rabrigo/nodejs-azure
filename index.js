3240b80bbb2adc12f8b572837f55d30a521a8721
the rawness <ronnelabrigo@gmail.com>
2023-10-24 19:49:12 -0700

    switch to first code

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-// const express = require('express');
-// const PORT = 3000;
-// const app = express();
+const express = require('express');
+const PORT = 3000;
+const app = express();
 
-// app.get('/', (req, res) => {
-//     res.json({ hi: 'it works' })
-// })
+app.get('/', (req, res) => {
+    res.json({ hi: 'it works' })
+})
 
-// app.get('/test', (req, res) => {
-//     res.json({ ok: true })
-// })
+app.get('/test', (req, res) => {
+    res.json({ ok: true })
+})
 
-// app.listen(PORT, () => console.log(`Now listening on http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`Now listening on http://localhost:${PORT}`));
 
-const http = require('http');
+// const http = require('http');
 
-const server = http.createServer((req, res) => {
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end('Confirming it works');
-});
+// const server = http.createServer((req, res) => {
+//     res.writeHead(200, { "Content-Type": "text/plain" });
+//     res.end('Confirming it works');
+// });
 
-const port = process.env.PORT || 3000;
-server.listen(port);
+// const port = process.env.PORT || 3000;
+// server.listen(port);
 
-console.log(`Server running at http://localhost:${port}`);
+// console.log(`Server running at http://localhost:${port}`);
